214aec08fca012455727457cdebee46157df7461
Shivey Singh <shivey_khangura@hotmail.com>
2021-02-01 15:33:47 +0000

    Add files via upload

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workoutschema.js");
 
 
 router.post("/api/workout", (req, res) => {
